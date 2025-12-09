ffdc8e55d9ac601329d6a956752e9f58f7ba81c2
Siddharth Kshetrapal <siddharthkp@github.com>
2025-12-09 18:21:48 +0530

    Add getSuggestionsLikeVariable (#42)
    * add getSuggestionsLikeVariable
    
    * doc is already defined

--- src/suggestions.test.ts
+++ src/suggestions.test.ts
@@ -1,9 +1,19 @@
 import {describe, it, expect} from 'vitest'
-import {getSuggestions} from './suggestions'
+import {getSuggestions, getSuggestionsLikeVariable} from './suggestions'
 
 describe('Suggestions', () => {
   it('returns suggestions for paddingBottom', () => {
     const suggestions = getSuggestions('paddingBottom')
     expect(suggestions).toMatchSnapshot()
+  })
+
+  it('returns suggestions for variable part: --fgColor', () => {
+    const suggestions = getSuggestionsLikeVariable('--fgColor')
+    expect(suggestions[0].name).toBe('--fgColor-accent')
+  })
+
+  it.todo('returns suggestions for property: outline', () => {
+    const suggestions = getSuggestions('outline')
+    expect(suggestions[0].name).toBe('--focus-outline')
   })
 })
